69c353b8fb5d841f775560cbfddb82bfbececcb9
Jimmy Rickardsson <Rickardssonjimmy@gmail.com>
2021-03-09 11:48:30 +0100

    Fixed so previous messages gets printed out in the right chatroom

--- routes/rooms.js
+++ routes/rooms.js
@@ -5,17 +5,39 @@ const NewRoomModel = require("../models/roomsModel")
 
 router.use(express.urlencoded({ extended: true }))
 
-router.get("/:room", (req, res) => {
+router.get("/:room", async (req, res) => {
   // NewRoomModel.find({}, "roomName", (error, rooms) => {
   //   if (error) return handleError(error)
   //   res.render("room.ejs", { rooms, room: req.params.room })
   // })
 
-  NewRoomModel.find((error, rooms) => {
-    console.log(rooms)
+  // NewRoomModel.find({}, (error, rooms) => {
+  //   console.log(rooms)
+  //   if (error) return handleError(error)
+  //   res.render("room.ejs", { rooms, room: req.params.room })
+  // })
+  // })
+
+  //FUNGRAR FÖR ATT FÅ FRAM ENDAST ETT RUM
+  // NewRoomModel.findOne({ roomName: req.params.room }, (error, thisRoom) => {
+  //   console.log(thisRoom)
+  //   if (error) return handleError(error)
+  //   res.render("room.ejs", { thisRoom })
+  // })
+
+  let rooms = []
+  let room = {}
+  await NewRoomModel.find({}, (error, data) => {
     if (error) return handleError(error)
-    res.render("room.ejs", { rooms, room: req.params.room })
+    rooms = data
   })
+
+  await NewRoomModel.findOne({ roomName: req.params.room }, (error, data) => {
+    if (error) return handleError(error)
+    room = data
+  })
+
+  res.render("room.ejs", { rooms, room })
 })
 
 module.exports = router
